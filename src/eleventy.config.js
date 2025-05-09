ca8ee93eae11d4543b5a1218a1eca660c73f44eb
OpenCo <admin@openco.ca>
2025-05-09 10:35:19 +0000

    feat: Add sectionize plugin and enhance date formatting filters in Eleventy config

--- src/eleventy.config.js
+++ src/eleventy.config.js
@@ -8,6 +8,16 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const sectionizePlugin = require("./_plugins/eleventy-plugin-sectionize");
 
 module.exports = function(eleventyConfig) {
+  // Add the sectionize plugin - direct invocation as it was designed
+  sectionizePlugin(eleventyConfig);
+  
+  // Add other plugins
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(pluginBundle);
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  
   eleventyConfig.addPassthroughCopy({
     "./assets/": "/",
   });
@@ -22,8 +32,20 @@ module.exports = function(eleventyConfig) {
   // _passthrough _redirects
   eleventyConfig.addPassthroughCopy("_redirects");
 
-  //have specifically md files use the default.njk layout
+  // have specifically md files use the default.njk layout if not specified
+  // This acts as a fallback for markdown files that don't specify a layout
+  eleventyConfig.addGlobalData("layout", "default.njk");
+  eleventyConfig.addWatchTarget("**/*.md");
 
+  // Date formatting (human readable)
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
+  // Date formatting (machine readable)
+  eleventyConfig.addFilter("machineDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy-MM-dd");
+  });
 
   return {
     dir: {
